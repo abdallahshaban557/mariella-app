e08f531c4c46b7970020bec0490b88380a4e57e8
abdullahshaaban <abdullah.shaaban91@gmail.com>
2017-11-26 20:36:02 -0800

    fixed end point again

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -64,9 +64,9 @@ export class AuthService {
   //Adjust this once a production app is live and ready
   prepEndpoint(ep){
     if(this.isDev){
-      return 'http://localhost:8080/'+ep;
-    } else {
       return ep;
+    } else {
+      return 'http://localhost:8080/'+ep;
     }
   }
 }
